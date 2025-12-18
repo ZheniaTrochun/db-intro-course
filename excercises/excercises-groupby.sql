5231527b81f395b974a48cdeaa86c5eaa62022b7
Daniil Leheza <lehesadanylo27@gmail.com>
2025-12-18 22:55:30 +0200

    feat(excercises): Add query to compare avg grade of student to avg grade of group

--- excercises/excercises-groupby.sql
+++ excercises/excercises-groupby.sql
@@ -5,7 +5,16 @@ JOIN enrolment e USING(course_id)
 GROUP BY c.student_year
 ORDER BY c.student_year
 -- для кожного з студентів знайти його середній бал у порівнянні з середнім балом по групі
---
+SELECT
+s.student_id,
+s.surname || ' ' || s.name AS "Студент",
+g.name AS "Група",
+ROUND(AVG(e.grade) OVER (PARTITION BY s.student_id), 1) AS "Середній бал студента",
+ROUND(AVG(e.grade) OVER (PARTITION BY s.group_id), 1) AS "Середній бал по групі"
+FROM student s
+JOIN student_group g USING (group_id)
+JOIN enrolment e USING (student_id)
+WHERE e.grade IS NOT NULL
 -- порахувати статистику записів на курси для кожного року навчання:
 --      кількість курсів
 --      кількість записів
