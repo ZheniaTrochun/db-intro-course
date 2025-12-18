1aea34498e7462bb1f8025ce667212ad295e6366
Daniil Leheza <lehesadanylo27@gmail.com>
2025-12-18 23:53:21 +0200

    feat(exercises): Finish last query in groupby.sql

--- excercises/excercises-groupby.sql
+++ excercises/excercises-groupby.sql
@@ -20,3 +20,12 @@ ORDER BY s.student_id
 --      кількість курсів
 --      кількість записів
 --      кількість студентів, що вже отримали бали
+SELECT
+c.student_year AS "Рік навчання",
+COUNT(DISTINCT c.course_id) AS "Кількість курсів",
+COUNT(e.student_id) AS "Кількість записів",
+COUNT(DISTINCT e.student_id) FILTER (WHERE e.grade IS NOT NULL) AS "Студентів з оцінками"
+FROM course c
+LEFT JOIN enrolment e ON c.course_id = e.course_id
+GROUP BY c.student_year
+ORDER BY c.student_year;
