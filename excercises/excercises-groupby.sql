f25e0ce95ecfd64ea5c08fc654191befe1149935
StudentPP1 <mishamakytonin@gmail.com>
2025-12-19 14:35:19 +0200

    feat: implement exercises

--- excercises/excercises-groupby.sql
+++ excercises/excercises-groupby.sql
@@ -1,9 +1,50 @@
 -- порахувати успішність студентів залежно від року навчання
---
+select 
+    c.student_year,
+    s.surname,
+    s.name,
+    g.name,
+    round(avg(e.grade), 2) as avg_grade
+from public.student as s
+join public.enrolment as e using (student_id)
+join public.course as c using (course_id)
+join public.student_group as g using (group_id)
+group by c.student_year, s.student_id, s.surname, s.name, g.name
+order by c.student_year asc, avg_grade desc;
+
 -- для кожного з студентів знайти його середній бал у порівнянні з середнім балом по групі
---
+with group_avg_grades as (
+    select  
+        group_id, 
+        avg(grade) as group_avg
+    from public.student
+    join public.enrolment using (student_id)
+    group by group_id
+)
+select 
+    s.surname,
+    s.name,
+    g.name,
+    round(avg(e.grade), 2) as student_avg,
+    round(gag.group_avg, 2) as group_avg,
+    round(avg(e.grade) - gag.group_avg, 2) as difference
+from public.student as s
+join public.enrolment as e using (student_id)
+join public.student_group as g using (group_id)
+join group_avg_grades as gag using (group_id)
+group by s.student_id, s.surname, s.name, g.name, gag.group_avg
+order by g.name, student_avg desc;
+
 -- порахувати статистику записів на курси для кожного року навчання:
 --      кількість курсів
 --      кількість записів
 --      кількість студентів, що вже отримали бали
-
+select 
+    c.student_year,
+    count(distinct c.course_id) as course_count,
+    count(e.student_id) as enrollment_count,
+    count(e.grade) as graded_students_count 
+from public.course as c
+left join public.enrolment as e using (course_id)
+group by c.student_year
+order by c.student_year;
--- excercises/excercises-subqueries.sql
+++ excercises/excercises-subqueries.sql
@@ -1,3 +1,63 @@
 -- Для кожного курсу знайти в якому мінімальному семестрі він може читатись
+with recursive course_hierarchy as (
+    select 
+        course_id, 
+        name, 
+        student_year::integer as min_year
+    from public.course
+    where course_id not in (select course_id from public.course_prerequisite)
+
+    union all
+
+    select 
+        c.course_id, 
+        c.name, 
+		-- якщо курс має пререквізит, то його можна взяти на наступному -> +1
+    	greatest(c.student_year, h.min_year + 1)
+    from public.course as c
+    join public.course_prerequisite as p on c.course_id = p.course_id
+    join course_hierarchy as h on p.prerequisite_course_id = h.course_id
+)
+select 
+    name, 
+    max(min_year) as earliest_possible_year
+from course_hierarchy
+group by name
+order by earliest_possible_year;
+
 -- Знайти всіх студентів, які записані на більше курсів ніж в середньому студенти
+with enrollment_stats as (
+	select count(*)::float / count(distinct student_id) as couse_enrolment_avg from public.enrolment
+)
+select s.name, s.surname, count(e.course_id)
+from public.enrolment as e
+join public.student as s using (student_id)
+group by s.student_id
+having count(e.course_id) > (select couse_enrolment_avg from enrollment_stats)
+order by s.student_id;
+
 -- Знайти топ-3 студенти у кожному курсі за отриманими балами
+with ranked_enrolments as (
+    select 
+        c.name as course_name,
+        s.surname,
+        s.name as student_name,
+        e.grade,
+        dense_rank() over (
+            partition by c.course_id 
+            order by e.grade desc
+        ) as student_rank
+    from public.enrolment as e
+    join public.student as s using (student_id)
+    join public.course as c using (course_id)
+    where e.grade is not null
+)
+select 
+    course_name,
+    surname,
+    student_name,
+    grade,
+    student_rank
+from ranked_enrolments
+where student_rank <= 3
+order by course_name, student_rank;
