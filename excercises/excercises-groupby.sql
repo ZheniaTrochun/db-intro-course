700b17bb72e38dab51b977b3b1b33a2cfcfd2659
StudentPP1 <mishamakytonin@gmail.com>
2025-12-19 21:48:23 +0200

    feat: refactor queries

--- excercises/excercises-groupby.sql
+++ excercises/excercises-groupby.sql
@@ -1,50 +1,57 @@
--- порахувати успішність студентів залежно від року навчання
+-- порахувати успішність студентів залежно від року навчання = avg grade for each student for each year
 select 
-    c.student_year,
-    s.surname,
-    s.name,
-    g.name,
-    round(avg(e.grade), 2) as avg_grade
+	c.student_year, 
+	s.name, 
+	s.surname, 
+	sg.name, 
+	COALESCE(round(avg(e.grade), 2), 0)
 from public.student as s
+join public.student_group as sg using (group_id)
 join public.enrolment as e using (student_id)
 join public.course as c using (course_id)
-join public.student_group as g using (group_id)
-group by c.student_year, s.student_id, s.surname, s.name, g.name
-order by c.student_year asc, avg_grade desc;
+where c.is_active = true
+-- sql standart obliges that all fields in select must be in group by  
+group by c.student_year, s.student_id, s.name, s.surname, sg.group_id, sg.name
+order by c.student_year, sg.group_id
 
 -- для кожного з студентів знайти його середній бал у порівнянні з середнім балом по групі
-with group_avg_grades as (
-    select  
-        group_id, 
-        avg(grade) as group_avg
-    from public.student
-    join public.enrolment using (student_id)
-    group by group_id
+with group_grade_avg as (
+	-- if null => 0 and also play in avg grade of group
+	select sg.group_id, AVG(COALESCE(e.grade, 0)) as avg_grade
+	from public.enrolment as e 
+	join public.student as s using (student_id)
+	join public.student_group as sg using (group_id)
+	group by sg.group_id
+	order by sg.group_id
 )
 select 
-    s.surname,
-    s.name,
-    g.name,
-    round(avg(e.grade), 2) as student_avg,
-    round(gag.group_avg, 2) as group_avg,
-    round(avg(e.grade) - gag.group_avg, 2) as difference
-from public.student as s
-join public.enrolment as e using (student_id)
-join public.student_group as g using (group_id)
-join group_avg_grades as gag using (group_id)
-group by s.student_id, s.surname, s.name, g.name, gag.group_avg
-order by g.name, student_avg desc;
+	s.name, 
+	s.surname, 
+	sg.name, 
+	-- if null => 0 and also play in avg grade for student
+	round(avg(coalesce(e.grade, 0)), 2) as student_avg,
+	round(gga.avg_grade, 2) as group_avg,
+	round(gga.avg_grade, 2) - round(avg(coalesce(e.grade, 0)), 2) as diff
+from public.enrolment as e  
+-- all student even if they don't have courses
+right join public.student as s using (student_id)
+join public.student_group as sg using (group_id)
+join group_grade_avg as gga using (group_id)
+group by s.student_id, s.name, s.surname, sg.group_id, sg.name, gga.avg_grade
+order by sg.name, student_avg desc;
 
 -- порахувати статистику записів на курси для кожного року навчання:
 --      кількість курсів
 --      кількість записів
 --      кількість студентів, що вже отримали бали
 select 
-    c.student_year,
-    count(distinct c.course_id) as course_count,
-    count(e.student_id) as enrollment_count,
-    count(e.grade) as graded_students_count 
-from public.course as c
-left join public.enrolment as e using (course_id)
+	c.student_year, 
+	count(distinct e.course_id) as course_count,
+	count(e.student_id) as student_count,
+	-- count igonore null fields
+	count(e.grade) as graded_students
+from public.enrolment as e
+-- all courses even without students
+right join public.course as c using (course_id)
 group by c.student_year
-order by c.student_year;
+order by c.student_year
--- excercises/excercises-subqueries.sql
+++ excercises/excercises-subqueries.sql
@@ -1,63 +1,58 @@
 -- Для кожного курсу знайти в якому мінімальному семестрі він може читатись
-with recursive course_hierarchy as (
+with recursive academic_path as (
+    -- сourses without prerequisites can be taken in year 1
     select 
         course_id, 
         name, 
-        student_year::integer as min_year
+        1 as theoretical_min_year
     from public.course
     where course_id not in (select course_id from public.course_prerequisite)
 
     union all
 
+    -- recursive part: prerequisite year + 1
     select 
         c.course_id, 
         c.name, 
-		-- якщо курс має пререквізит, то його можна взяти на наступному -> +1
-    	greatest(c.student_year, h.min_year + 1)
+        ap.theoretical_min_year + 1
     from public.course as c
     join public.course_prerequisite as p on c.course_id = p.course_id
-    join course_hierarchy as h on p.prerequisite_course_id = h.course_id
+    join academic_path as ap on p.prerequisite_course_id = ap.course_id
 )
 select 
     name, 
-    max(min_year) as earliest_possible_year
-from course_hierarchy
+    max(theoretical_min_year) as earliest_possible_year
+from academic_path
 group by name
 order by earliest_possible_year;
 
 -- Знайти всіх студентів, які записані на більше курсів ніж в середньому студенти
-with enrollment_stats as (
-	select count(*)::float / count(distinct student_id) as couse_enrolment_avg from public.enrolment
+with course_stats as (
+	select 
+	-- one row in enrolment = one course for student => all courses / unique student
+	-- numeric = float number with strict position of .
+	    round(count(course_id)::numeric / count(distinct student_id), 2) as avg_courses_per_student
+	from public.enrolment
 )
-select s.name, s.surname, count(e.course_id)
+select s.name, s.surname, count(e.course_id) as courses
 from public.enrolment as e
-join public.student as s using (student_id)
-group by s.student_id
-having count(e.course_id) > (select couse_enrolment_avg from enrollment_stats)
-order by s.student_id;
+right join public.student as s using (student_id)
+cross join course_stats as cs
+group by s.student_id, s.name, s.surname, cs.avg_courses_per_student
+having count(e.course_id) > cs.avg_courses_per_student
 
 -- Знайти топ-3 студенти у кожному курсі за отриманими балами
-with ranked_enrolments as (
-    select 
-        c.name as course_name,
-        s.surname,
-        s.name as student_name,
-        e.grade,
-        dense_rank() over (
-            partition by c.course_id 
-            order by e.grade desc
-        ) as student_rank
-    from public.enrolment as e
-    join public.student as s using (student_id)
-    join public.course as c using (course_id)
-    where e.grade is not null
+with ranked_data as (
+	select 
+		s.name,
+		s.surname,
+		c.name as course_name,
+		coalesce(e.grade, 0) as grade,
+		dense_rank() over (partition by c.course_id order by coalesce(e.grade, 0) desc) as student_rank
+	from public.student as s
+	inner join public.enrolment as e using (student_id)
+	inner join public.course as c using (course_id)
 )
-select 
-    course_name,
-    surname,
-    student_name,
-    grade,
-    student_rank
-from ranked_enrolments
+select * from ranked_data 
 where student_rank <= 3
 order by course_name, student_rank;
