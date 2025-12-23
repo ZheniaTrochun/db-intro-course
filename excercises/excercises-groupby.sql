8ece6043063b1c91d39c89658e55312acc591b8c
AleksD1203 <smiley3223337@gmail.com>
2025-12-23 12:53:30 +0200

    Exercises done

--- excercises/excercises-groupby.sql
+++ excercises/excercises-groupby.sql
@@ -1,9 +1,35 @@
+--Всі запити та дані використані з файлів aggregations.sql, create-campus-tables.sql, insert-data.sql
 -- порахувати успішність студентів залежно від року навчання
+SELECT start_year, ROUND(AVG(grade), 1) AS avg_grade
+FROM student
+  JOIN enrolment USING (student_id)
+  JOIN student_group USING (group_id)
+WHERE grade IS NOT NULL
+GROUP BY start_year
+ORDER BY start_year;
 --
 -- для кожного з студентів знайти його середній бал у порівнянні з середнім балом по групі
 --
+SELECT DISTINCT
+    s.name || ' ' || s.surname AS full_name,
+    ROUND(AVG(grade) OVER (PARTITION BY s.student_id), 1) AS my_avg,
+    AVG(grade) OVER (PARTITION BY s.group_id) AS group_avg
+FROM student s
+    JOIN enrolment e ON s.student_id = e.student_id
+    JOIN student_group g ON s.group_id = g.group_id
+WHERE grade IS NOT NULL
+ORDER BY full_name;
 -- порахувати статистику записів на курси для кожного року навчання:
 --      кількість курсів
 --      кількість записів
 --      кількість студентів, що вже отримали бали
+SELECT start_year,
+  COUNT(DISTINCT course_id) AS courses,
+  COUNT(*) AS total_recs,
+  COUNT(grade) AS passed
+FROM student_group 
+  INNER JOIN student USING (group_id)
+  INNER JOIN enrolment USING (student_id)
+GROUP BY start_year
+ORDER BY start_year;
 
--- excercises/excercises-subqueries.sql
+++ excercises/excercises-subqueries.sql
@@ -1,3 +1,34 @@
+--Всі запити та дані використані з файлів create-campus-tables.sql, insert-data.sql, subqueries.sql
 -- Для кожного курсу знайти в якому мінімальному семестрі він може читатись
+WITH RECURSIVE course_sem AS (
+   SELECT course_id, 1 AS sem FROM course
+   WHERE course_id NOT IN (SELECT course_id FROM course_prerequisite)
+   UNION ALL
+   SELECT cp.course_id, t.sem + 1
+   FROM course_prerequisite cp
+   JOIN course c ON c.course_id = cp.course_id
+   JOIN course_sem t ON t.course_id = cp.prerequisite_course_id
+)
+SELECT name, MAX(sem) AS min_sem 
+FROM course_sem 
+JOIN course ON course_sem.course_id = course.course_id
+GROUP BY name 
+ORDER BY min_sem;
 -- Знайти всіх студентів, які записані на більше курсів ніж в середньому студенти
+WITH counts AS (
+  SELECT student_id, COUNT(*) AS count_val
+  FROM enrolment GROUP BY student_id
+)
+SELECT * FROM student s
+INNER JOIN counts c ON s.student_id = c.student_id
+WHERE c.count_val > (SELECT AVG(count_val) FROM counts);
 -- Знайти топ-3 студенти у кожному курсі за отриманими балами
+WITH rank AS (
+   SELECT c.name AS course, surname, grade,
+   ROW_NUMBER() OVER (PARTITION BY e.course_id ORDER BY grade DESC) AS row_number
+   FROM enrolment e
+   JOIN student s USING(student_id)
+   JOIN course c USING(course_id)
+   WHERE grade IS NOT NULL AND grade > 0
+)
+SELECT * FROM rank WHERE row_number <= 3;
