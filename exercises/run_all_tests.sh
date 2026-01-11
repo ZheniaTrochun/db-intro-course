18807bc590151f7a67dc675523930e4af144586d
Yevhenii Trochun <yevhenii.trochun@lotusflare.com>
2026-01-11 21:02:07 +0200

    corrected spelling

--- exercises/run_all_tests.sh
+++ exercises/run_all_tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ ! -f ./test_runner/venv ]; then
-  python3 -m venv ./test_runner/venv
-  source ./test_runner/venv/bin/activate
-  pip3 install -r ./test_runner/requirements.txt
+if [ ! -f ./tests/venv/bin/activate ]; then
+  python3 -m venv ./tests/venv
+  source ./tests/venv/bin/activate
+  pip3 install -r ./tests/requirements.txt
 fi
 
-cd ./test_runner || exit
+cd ./tests || exit
 
 pytest --html=test_results/report.html --json-report --json-report-file=test_results/report.json --snapshot base --no-header -v
 pytest --html=test_results/report.html --json-report --json-report-file=test_results/report.json --snapshot 10k --no-header -v
