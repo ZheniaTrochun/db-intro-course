ebc80601560f3588d16a85de3ba016390b8edb1c
Denis Orlyk <denisorlyk20@gmail.com>
2026-04-01 19:56:24 +0300

    JOINS exercises

--- exercises/joins/professor_not_curator.sql
+++ exercises/joins/professor_not_curator.sql
@@ -7,3 +7,9 @@
 --          - повним іменем викладача
 
 -- Рішення:
+SELECT p.first_name || ' ' || p.last_name AS professor_name, prof.job AS job
+FROM professor prof
+LEFT JOIN person p ON p.person_id = prof.person_id
+LEFT JOIN student_group sg ON sg.curator_id = prof.professor_id
+WHERE prof.status = 'викладає' and sg.curator_id IS NULL
+ORDER BY professor_name;
