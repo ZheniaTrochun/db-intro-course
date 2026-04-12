9896738a63f01db11767b3f9d3c3ebc27af0576c
termenatorof <artemseremeta487@gmail.com>
2026-04-12 18:52:58 +0300

    Update course_prerequisites.sql

--- exercises/joins/course_prerequisites.sql
+++ exercises/joins/course_prerequisites.sql
@@ -8,3 +8,12 @@
 --          - назвою курсу, потім за назвою пре-реквізиту
 
 -- Рішення:
+SELECT 
+    c1.name AS course_name,
+    c2.name AS prerequisite_name
+FROM course c1
+LEFT JOIN course_prerequisite cp ON c1.id = cp.course_id
+LEFT JOIN course c2 ON cp.prerequisite_course_id = c2.id
+ORDER BY 
+    course_name ASC, 
+    prerequisite_name ASC;
