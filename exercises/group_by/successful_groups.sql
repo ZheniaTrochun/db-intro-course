f693c59138cd0ad904dc2f3831c6959fa59952a6
termenatorof <artemseremeta487@gmail.com>
2026-04-12 18:58:15 +0300

    Update successful_groups.sql

--- exercises/group_by/successful_groups.sql
+++ exercises/group_by/successful_groups.sql
@@ -8,3 +8,15 @@
 --          - середнім балом (спадання), потім за назвою групи
 
 -- Рішення:
+SELECT 
+    sg.name AS group_name,
+    COUNT(DISTINCT s.id) AS student_count,
+    ROUND(AVG(e.grade), 2) AS avg_grade
+FROM student_group sg
+JOIN student s ON sg.id = s.group_id
+JOIN enrolment e ON s.id = e.student_id
+GROUP BY sg.id, sg.name
+HAVING AVG(e.grade) > 75
+ORDER BY 
+    avg_grade DESC, 
+    group_name ASC;
