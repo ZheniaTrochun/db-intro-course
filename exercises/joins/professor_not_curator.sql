3271358c6f7d6f8e73a838b55f1eb91a2a8ff5ea
termenatorof <artemseremeta487@gmail.com>
2026-04-12 18:46:24 +0300

    Update professor_not_curator.sql

--- exercises/joins/professor_not_curator.sql
+++ exercises/joins/professor_not_curator.sql
@@ -7,3 +7,12 @@
 --          - повним іменем викладача
 
 -- Рішення:
+SELECT 
+    p.first_name || ' ' || p.last_name AS professor_name,
+    pr.job
+FROM professor pr
+JOIN person p ON pr.person_id = p.id
+LEFT JOIN student_group sg ON pr.id = sg.curator_id
+WHERE pr.status = 'викладає' 
+  AND sg.id IS NULL
+ORDER BY professor_name;
