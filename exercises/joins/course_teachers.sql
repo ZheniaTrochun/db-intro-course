b320fa8664037b27b8b4011e2595757799ccfd60
termenatorof <artemseremeta487@gmail.com>
2026-04-12 18:32:12 +0300

    Шеремета Артем ІО-46 course_teachers.sql

--- exercises/joins/course_teachers.sql
+++ exercises/joins/course_teachers.sql
@@ -9,3 +9,13 @@
 --          - назвою курсу, потім за роллю викладача
 
 -- Рішення:
+
+SELECT 
+    c.course_name, 
+    p.professor_name, 
+    ct.role
+FROM courses c
+JOIN course_teachers ct ON c.id = ct.course_id
+JOIN professors p ON ct.professor_id = p.id
+WHERE c.status = 'активний'
+ORDER BY c.course_name ASC, ct.role ASC;
