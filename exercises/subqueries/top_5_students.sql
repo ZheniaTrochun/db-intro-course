d2081f4f0cd2a8ca1092b58e5bf28f64d13f2c81
termenatorof <artemseremeta487@gmail.com>
2026-04-12 20:34:53 +0300

    Update top_5_students.sql

--- exercises/subqueries/top_5_students.sql
+++ exercises/subqueries/top_5_students.sql
@@ -16,8 +16,7 @@ WITH RankedStudents AS (
         s.student_id AS student_id,
         p.first_name || ' ' || p.last_name AS student_full_name,
         e.grade,
-        DENSE_RANK() OVER (PARTITION BY c.course_id ORDER BY e.grade DESC) AS rank,
-        ROW_NUMBER() OVER (PARTITION BY c.course_id ORDER BY e.grade DESC, p.first_name || ' ' || p.last_name ASC) AS rn
+        ROW_NUMBER() OVER (PARTITION BY c.course_id ORDER BY e.grade DESC, p.first_name || ' ' || p.last_name ASC) AS rank
     FROM enrolment e
     JOIN course c ON e.course_id = c.course_id
     JOIN student s ON e.student_id = s.student_id
@@ -31,7 +30,7 @@ SELECT
     grade,
     rank
 FROM RankedStudents
-WHERE rn <= 5
+WHERE rank <= 5
 ORDER BY 
     course_name ASC, 
     rank ASC, 
