5f4a20229833964a84dbd23d362dd21a76528a76
termenatorof <artemseremeta487@gmail.com>
2026-04-12 19:18:03 +0300

    Update professor_not_curator.sql

--- exercises/joins/professor_not_curator.sql
+++ exercises/joins/professor_not_curator.sql
@@ -11,8 +11,8 @@ SELECT
     p.first_name || ' ' || p.last_name AS professor_name,
     pr.job
 FROM professor pr
-JOIN person p ON pr.person_id = p.id
-LEFT JOIN student_group sg ON pr.id = sg.curator_id
+JOIN person p ON pr.person_id = p.person_id
+LEFT JOIN student_group sg ON pr.professor_id = sg.curator_id
 WHERE pr.status = 'викладає' 
-  AND sg.id IS NULL
+  AND sg.group_id IS NULL
 ORDER BY professor_name;
