5b80a3eb66e16911fdaa88d4d2f924b8406d2084
kostenkodmitro98-cmyk <kostenkodmitro98@gmail.com>
2026-04-13 17:36:18 +0300

    Update course_prerequisites.sql

--- exercises/joins/course_prerequisites.sql
+++ exercises/joins/course_prerequisites.sql
@@ -8,3 +8,10 @@
 --          - назвою курсу, потім за назвою пре-реквізиту
 
 -- Рішення:
+SELECT 
+    c.name AS course_name,
+    p.name AS prerequisite_name
+FROM courses c
+LEFT JOIN course_prerequisites cp ON c.id = cp.course_id
+LEFT JOIN courses p ON cp.prerequisite_id = p.id
+ORDER BY course_name ASC, prerequisite_name ASC;
