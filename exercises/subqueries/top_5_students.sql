ee50ff92020ea9a09ff984026e543c3f06f57062
Mars-back <marianboy31@gmail.com>
2026-04-24 15:10:41 +0300

    Exercises

--- exercises/subqueries/top_5_students.sql
+++ exercises/subqueries/top_5_students.sql
@@ -10,3 +10,27 @@
 --          - назвою курсу, потім за рангом (зростання), потім за іменем студента
 
 -- Рішення:
+with ranked_students as (
+    select
+        c.name as course_name,
+        s.student_id,
+        p.first_name || ' ' || p.last_name as student_full_name,
+        e.grade,
+        row_number() over (
+            partition by c.course_id 
+            order by e.grade desc nulls last, p.first_name || ' ' || p.last_name asc, s.student_id asc
+        ) as rank
+    from student s
+    inner join person p on p.person_id = s.person_id
+    inner join enrolment e on e.student_id = s.student_id
+    inner join course c on c.course_id = e.course_id
+)
+select 
+    course_name,
+    student_id,
+    student_full_name,
+    grade,
+    rank
+from ranked_students
+where rank <= 5
+order by course_name, rank asc, student_full_name;
