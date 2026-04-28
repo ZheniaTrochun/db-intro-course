586d6f21aa3d8220463b2ea4ecac26b7407c751a
termenatorof <artemseremeta487@gmail.com>
2026-04-28 18:14:26 +0300

    Update min_semester_for_course.sql

--- exercises/subqueries/min_semester_for_course.sql
+++ exercises/subqueries/min_semester_for_course.sql
@@ -8,17 +8,3 @@
 --          - мінімальним роком (зростання), потім за назвою курсу
 
 -- Рішення:
-SELECT 
-    c.course_id AS course_id,
-    c.name,
-    (
-        SELECT MIN(e.start_year - sg.start_year + 1)
-        FROM enrolment e
-        JOIN student s ON e.student_id = s.student_id
-        JOIN student_group sg ON s.group_id = sg.group_id
-        WHERE e.course_id = c.course_id AND e.grade IS NOT NULL
-    ) AS min_year
-FROM course c
-ORDER BY 
-    min_year ASC, 
-    c.name ASC;
