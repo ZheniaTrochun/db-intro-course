5804c416e42c1d78ba218b62b8b88a0386c81ed1
Reaftepi <pavlenkoartur882@gmail.com>
2026-04-28 18:28:28 +0300

    вправа 1 - students_at_risk

--- exercises/joins/students_at_risk.sql
+++ exercises/joins/students_at_risk.sql
@@ -12,3 +12,21 @@
 --          - оцінкою (зростання), потім за назвою групи, потім за іменем студента, потім за назвою курсу
 
 -- Рішення:
+SELECT 
+ ps.first_name || ' ' || ps.last_name as student_name,
+ sg.name as group_name,
+ c.name as course_name,
+ ROUND(e.grade, 2) as grade,
+ pp.first_name || ' ' || pp.last_name as lecturer_name
+FROM enrolment e
+JOIN student s on e.student_id = s.student_id
+JOIN person ps on s.person_id = ps.person_id
+JOIN student_group sg on s.group_id = sg.group_id
+JOIN course c on e.course_id = c.course_id
+JOIN course_teacher ct on c.course_id = ct.course_id
+JOIN professor prof on ct.professor_id = prof.professor_id
+JOIN person pp on prof.person_id = pp.person_id
+WHERE e.grade < 60 
+ AND e.grade IS NOT NULL
+ AND ct.professor_role::text = 'лектор'
+ORDER BY grade ASC, group_name ASC, student_name ASC, course_name ASC;
