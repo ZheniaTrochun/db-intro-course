5ec536b33e57fe7a80e449640c9570cb2c99e610
korsunBohdan <korsunb700@gmail.com>
2026-04-29 20:01:33 +0300

    Виконано перші 6 вправ

--- exercises/joins/course_teachers.sql
+++ exercises/joins/course_teachers.sql
@@ -9,3 +9,13 @@
 --          - назвою курсу, потім за роллю викладача
 
 -- Рішення:
+SELECT c.name as "course_name", p.first_name || ' ' || p.last_name as "professor_name", ct.professor_role as "role"
+FROM course c
+	JOIN course_teacher ct
+		on c.course_id = ct.course_id
+	JOIN professor pr
+		on ct.professor_id = pr.professor_id
+	JOIN person p
+		on pr.person_id = p.person_id
+WHERE c.status = 'активний'
+ORDER BY c.name, ct.professor_role;
