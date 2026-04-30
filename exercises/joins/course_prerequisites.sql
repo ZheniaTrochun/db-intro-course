f5ecacecdaf56f500fd6ff50d1b878aafadb24d4
ARTEM-SERDIUK <serdiuk.artem@lll.kpi.ua>
2026-04-30 13:26:48 +0300

    Виконано joins

--- exercises/joins/course_prerequisites.sql
+++ exercises/joins/course_prerequisites.sql
@@ -8,3 +8,8 @@
 --          - назвою курсу, потім за назвою пре-реквізиту
 
 -- Рішення:
+SELECT c.name as course_name, c2.name as prerequisite_name
+FROM course c
+LEFT JOIN course_prerequisite cp ON c.course_id = cp.course_id
+LEFT JOIN course c2 ON cp.prerequisite_course_id = c2.course_id
+ORDER BY course_name, prerequisite_name
--- exercises/joins/course_teachers.sql
+++ exercises/joins/course_teachers.sql
@@ -9,3 +9,11 @@
 --          - назвою курсу, потім за роллю викладача
 
 -- Рішення:
+SELECT c.name as course_name, ppr.first_name || ' ' || ppr.last_name as teacher_name,
+ct.professor_role as role
+FROM course c
+JOIN course_teacher ct ON c.course_id = ct.course_id
+JOIN professor pr ON ct.professor_id = pr.professor_id
+JOIN person ppr ON pr.person_id = ppr.person_id
+WHERE c.status = 'активний'
+ORDER BY course_name, role
