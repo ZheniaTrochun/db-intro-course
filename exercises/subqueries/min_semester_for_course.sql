38eaf2226898e647c3384ee29769d365a5ce7335
ARTEM-SERDIUK <serdiuk.artem@lll.kpi.ua>
2026-04-30 13:28:44 +0300

    Виконано subqueries

--- exercises/subqueries/min_semester_for_course.sql
+++ exercises/subqueries/min_semester_for_course.sql
@@ -8,3 +8,20 @@
 --          - мінімальним роком (зростання), потім за назвою курсу
 
 -- Рішення:
+WITH RECURSIVE course_levels AS (
+    SELECT c.course_id, c.name, 1 AS min_year
+    FROM course c
+    WHERE c.course_id NOT IN (SELECT cp.course_id FROM course_prerequisite cp)
+
+    UNION ALL
+
+    SELECT c.course_id, c.name, cl.min_year + 1
+    FROM course c
+    JOIN course_prerequisite cp ON c.course_id = cp.course_id
+    JOIN course_levels cl ON cp.prerequisite_course_id = cl.course_id
+)
+SELECT cl.course_id as course_id, cl.name as name, MAX(cl.min_year) AS min_year
+FROM course_levels cl
+WHERE EXISTS (SELECT 1 FROM enrolment e WHERE e.course_id = cl.course_id)
+GROUP BY course_id, name
+ORDER BY min_year, name
--- exercises/subqueries/students_with_many_enrolments.sql
+++ exercises/subqueries/students_with_many_enrolments.sql
@@ -9,3 +9,15 @@
 --          - кількістю курсів студента (спадання), потім за іменем студента
 
 -- Рішення:
+SELECT s.student_id AS student_id, p.first_name || ' ' || p.last_name AS full_name, 
+COUNT(e.course_id) AS course_number, (SELECT ROUND(AVG(counts.cnt), 2)
+	FROM (SELECT COUNT(course_id) AS cnt
+		FROM enrolment GROUP BY student_id) AS counts) AS avg_number
+FROM student s
+JOIN person p ON s.person_id = p.person_id
+JOIN enrolment e ON s.student_id = e.student_id
+GROUP BY s.student_id, p.first_name, p.last_name
+HAVING COUNT(e.course_id) > (SELECT AVG(counts.cnt)
+	FROM (SELECT COUNT(course_id) AS cnt
+		FROM enrolment GROUP BY student_id) AS counts)
+ORDER BY course_number DESC, full_name
