6ff62e20d7490346a29b04f2473a1c64e00dd4e2
ARTEM-SERDIUK <serdiuk.artem@lll.kpi.ua>
2026-04-30 13:27:52 +0300

    Виконано group_by

--- exercises/group_by/student_average_grade.sql
+++ exercises/group_by/student_average_grade.sql
@@ -10,3 +10,12 @@
 --          - за назвою групи, потім за іменем студента
 
 -- Рішення:
+SELECT s.student_id as student_id, p.first_name || ' ' || p.last_name as full_name,
+ROUND(AVG(e.grade), 2) as avg_student_grade, sg.name as group_name, 
+ROUND(AVG(AVG(e.grade)) OVER (PARTITION BY sg.group_id), 2) as avg_group_grade
+FROM student s
+JOIN person p ON s.person_id = p.person_id
+LEFT JOIN enrolment e ON s.student_id = e.student_id
+JOIN student_group sg ON s.group_id = sg.group_id
+GROUP BY s.student_id, full_name, sg.group_id, group_name
+ORDER BY group_name, full_name
