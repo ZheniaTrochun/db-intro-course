496b29f1126fcfa8d0a2a9b5cccaecb2de916a42
chubarevadarina-afk <chubareva.darina@lll.kpi.ua>
2026-05-02 13:49:51 +0300

    Add files via upload

--- exercises/joins/students_at_risk.sql
+++ exercises/joins/students_at_risk.sql
@@ -12,3 +12,26 @@
 --          - оцінкою (зростання), потім за назвою групи, потім за іменем студента, потім за назвою курсу
 
 -- Рішення:
+SELECT 
+    sp.first_name || ' ' || sp.last_name AS student_name,
+    sg.name AS group_name,
+    c.name AS course_name,
+    e.grade,
+    pp.first_name || ' ' || pp.last_name AS lecturer_name
+FROM enrolment e
+JOIN student s ON e.student_id = s.student_id
+JOIN person sp ON s.person_id = sp.person_id
+JOIN student_group sg ON s.group_id = sg.group_id
+JOIN course c ON e.course_id = c.course_id
+JOIN course_teacher ct ON c.course_id = ct.course_id
+JOIN professor prof ON ct.professor_id = prof.professor_id
+JOIN person pp ON prof.person_id = pp.person_id
+WHERE 
+    e.grade < 60 
+    AND e.grade IS NOT NULL 
+    AND ct.professor_role = 'лектор'
+ORDER BY 
+    e.grade ASC, 
+    group_name ASC, 
+    student_name ASC, 
+    course_name ASC;
