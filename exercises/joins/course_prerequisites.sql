c65a8e5c88ded13d2788b80f8b24053fbe85d92d
Zahar <ferenchuk.zahar@lll.kpi.ua>
2026-05-02 15:16:51 +0300

    Fix

--- exercises/joins/course_prerequisites.sql
+++ exercises/joins/course_prerequisites.sql
@@ -10,8 +10,8 @@
 -- Рішення:
 SELECT 
     c.name AS course_name,
-    pr.name AS prerequisite_course_name
+    pr.name AS prerequisite_name
 FROM course c
 JOIN course_prerequisite cp ON c.course_id = cp.course_id
 JOIN course pr ON cp.prerequisite_course_id = pr.course_id
-ORDER BY course_name ASC, prerequisite_course_name ASC;
+ORDER BY course_name ASC, prerequisite_name ASC;
--- exercises/joins/university_activities.sql
+++ exercises/joins/university_activities.sql
@@ -13,13 +13,19 @@
 -- Рішення:
 SELECT 
     p.first_name || ' ' || p.last_name AS full_name,
+    c.name AS course_name,
     'student' AS activity_type
-FROM student s
+FROM enrolment e
+JOIN student s ON e.student_id = s.student_id
 JOIN person p ON s.person_id = p.person_id
+JOIN course c ON e.course_id = c.course_id
 UNION
 SELECT 
     p.first_name || ' ' || p.last_name AS full_name,
+    c.name AS course_name,
     'professor' AS activity_type
-FROM professor prof
+FROM course_teacher ct
+JOIN professor prof ON ct.professor_id = prof.professor_id
 JOIN person p ON prof.person_id = p.person_id
-ORDER BY full_name ASC, activity_type ASC;
+JOIN course c ON ct.course_id = c.course_id
+ORDER BY full_name ASC, course_name ASC, activity_type ASC;
