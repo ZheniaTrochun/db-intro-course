65f4a4eb44c56ea1f129ac6f76df0faa25d6acfe
chubarevadarina-afk <chubareva.darina@lll.kpi.ua>
2026-05-04 18:06:20 +0300

    Update min_semester_for_course.sql

--- exercises/subqueries/min_semester_for_course.sql
+++ exercises/subqueries/min_semester_for_course.sql
@@ -11,10 +11,9 @@
 SELECT 
     c.course_id,
     c.name,
-    MIN(e.start_year - sg.start_year + 1) AS min_year
+    MIN(CAST(EXTRACT(YEAR FROM e.enrolment_date) - EXTRACT(YEAR FROM s.start_date) + 1 AS INTEGER)) AS min_year
 FROM course c
 JOIN enrolment e ON c.course_id = e.course_id
 JOIN student s ON e.student_id = s.student_id
-JOIN student_group sg ON s.group_id = sg.group_id
 GROUP BY c.course_id, c.name
 ORDER BY min_year ASC, c.name ASC;
