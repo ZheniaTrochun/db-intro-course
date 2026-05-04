667835c8fd0bdee9241cb95ee2e1b77c1b1f1523
chubarevadarina-afk <chubareva.darina@lll.kpi.ua>
2026-05-04 12:50:24 +0300

    Update student_success_rate.sql

--- exercises/group_by/student_success_rate.sql
+++ exercises/group_by/student_success_rate.sql
@@ -8,10 +8,9 @@
 
 -- Рішення:
 SELECT 
-    sg.admission_year AS student_year,
+    s.admission_year AS student_year,
     ROUND(AVG(e.grade), 2) AS avg_year_grade
 FROM student s
-JOIN student_group sg ON s.group_id = sg.group_id
 JOIN enrolment e ON s.student_id = e.student_id
-GROUP BY sg.admission_year
+GROUP BY s.admission_year
 ORDER BY student_year ASC;
