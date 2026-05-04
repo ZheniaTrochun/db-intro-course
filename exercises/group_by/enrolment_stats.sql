f081d862848b6d589b0ad6c851b4857f74771617
chubarevadarina-afk <chubareva.darina@lll.kpi.ua>
2026-05-04 12:58:11 +0300

    Update enrolment_stats.sql

--- exercises/group_by/enrolment_stats.sql
+++ exercises/group_by/enrolment_stats.sql
@@ -10,12 +10,11 @@
 
 -- Рішення:
 SELECT 
-    sg.admission_year AS student_year,
+    s.admission_year AS student_year,
     COUNT(DISTINCT e.course_id) AS number_of_courses,
     COUNT(e.student_id) AS number_of_enrolments,
     COUNT(e.grade) AS number_of_students_with_grade
 FROM student s
-JOIN student_group sg ON s.group_id = sg.group_id
 JOIN enrolment e ON s.student_id = e.student_id
-GROUP BY sg.admission_year
+GROUP BY s.admission_year
 ORDER BY student_year ASC;
