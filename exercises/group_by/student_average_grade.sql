51bcc89a04bec7798d857d8235950ca608a97a08
0xVShO <vlad0xbdev@gmail.com>
2026-05-04 16:39:56 +0300

    Fix Issues with tasks

--- exercises/group_by/student_average_grade.sql
+++ exercises/group_by/student_average_grade.sql
@@ -9,25 +9,39 @@
 --      Результат відсортувати за:
 --          - за назвою групи, потім за іменем студента
 
+-- Зміни:
+-- Додано CTE group_true_avg для обчислення середнього балу по групі
+-- CTE student_individual_avg обчислює середній бал для кожного студента
+-- Додано функцію CAST(... AS FLOAT) для коректного проходження автоматичного тестування, яке очікує саме FLOAT
+
 -- Рішення:
-WITH student_avg AS (
+WITH student_individual_avg AS (
     SELECT 
         s.student_id, 
         p.first_name || ' ' || p.last_name as full_name,
         sg.name as group_name,
-        AVG(e.grade) as avg_student_no_rounded_grade
+        s.group_id,
+        AVG(e.grade) as raw_student_avg
     FROM student as s
     JOIN person as p ON s.person_id = p.person_id
     JOIN student_group as sg ON s.group_id = sg.group_id
     LEFT JOIN enrolment as e ON e.student_id = s.student_id
     GROUP BY s.student_id, p.first_name, p.last_name, s.group_id, sg.name
+),
+group_true_avg AS (
+    SELECT 
+        group_id,
+        AVG(raw_student_avg) as raw_group_avg
+    FROM student_individual_avg
+    GROUP BY group_id
 )
 
 SELECT 
     sa.student_id, 
     sa.full_name, 
-    ROUND(sa.avg_student_no_rounded_grade, 2) as avg_student_grade,
+    CAST(ROUND(CAST(sa.raw_student_avg AS numeric), 2) AS FLOAT) as avg_student_grade,
     sa.group_name, 
-    ROUND(AVG(sa.avg_student_no_rounded_grade) OVER (PARTITION BY sa.group_name), 2) as avg_group_grade
-FROM student_avg as sa
+    CAST(ROUND(CAST(ga.raw_group_avg AS numeric), 2) AS FLOAT) as avg_group_grade
+FROM student_individual_avg sa
+LEFT JOIN group_true_avg ga ON sa.group_id = ga.group_id
 ORDER BY sa.group_name, sa.full_name;
--- exercises/group_by/successful_groups.sql
+++ exercises/group_by/successful_groups.sql
@@ -12,15 +12,19 @@
 -- бо у початковій версії запиту я враховував навіть тих студентів, у котрих не було оцінок:)
 -- У початковому запиті не було рядку "WHERE e.grade IS NOT NULL"
 
+-- Зміни:
+-- Додано функцію CAST (... AS FLOAT), так як автоматичне тестування PR очікує саме FLOAT
+-- Змінено знак в рядку HAVING AVG(e.grade) >= 75 на >=, так як в умові завдання вказано "вищий за 75", що означає "більше або дорівнює 75".  
+
 -- Рішення:
 SELECT
     sg.name as group_name,
     COUNT(DISTINCT s.student_id) AS student_count,
-    ROUND(AVG(e.grade), 2) AS avg_grade
+    CAST(ROUND(AVG(e.grade), 2) AS FLOAT) AS avg_grade
 FROM enrolment e 
 JOIN student s ON e.student_id = s.student_id
 JOIN student_group sg ON s.group_id = sg.group_id
 WHERE e.grade IS NOT NULL
 GROUP BY group_name
-HAVING AVG(e.grade) > 75
+HAVING AVG(e.grade) >= 75
 ORDER BY avg_grade DESC, group_name;
