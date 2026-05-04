7c87e157e4cff5228c50c332f4507f1f254f0da6
chubarevadarina-afk <chubareva.darina@lll.kpi.ua>
2026-05-04 16:53:16 +0300

    Update enrolment_stats.sql

--- exercises/group_by/enrolment_stats.sql
+++ exercises/group_by/enrolment_stats.sql
@@ -9,4 +9,13 @@
 --          - роком навчання (зростання)
 
 -- Рішення:
-SELECT * FROM student_group LIMIT 1;
+SELECT 
+    sg.start_year AS student_year,
+    COUNT(DISTINCT e.course_id) AS number_of_courses,
+    COUNT(e.student_id) AS number_of_enrolments,
+    COUNT(e.grade) AS number_of_students_with_grade
+FROM student_group sg
+JOIN student s ON sg.group_id = s.group_id
+JOIN enrolment e ON s.student_id = e.student_id
+GROUP BY sg.start_year
+ORDER BY student_year ASC;
