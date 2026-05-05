de2f488bdf86f4cf44a0ac30f2b04c03695eb5f5
Artem Cherevatov <_email@example.com>
2026-05-05 21:32:19 +0300

    Exercises з subqueries

--- exercises/subqueries/min_semester_for_course.sql
+++ exercises/subqueries/min_semester_for_course.sql
@@ -8,3 +8,20 @@
 --          - мінімальним роком (зростання), потім за назвою курсу
 
 -- Рішення:
+WITH RECURSIVE course_dependencies AS (
+    SELECT course_id, 1 as level
+    FROM course c
+    WHERE NOT EXISTS (
+        SELECT 1 
+        FROM course_prerequisite p 
+        WHERE p.course_id = c.course_id)
+    UNION ALL
+    SELECT p.course_id as course_id, 
+    cd.level + 1 as level
+    FROM course_prerequisite p
+             INNER JOIN course_dependencies cd ON p.prerequisite_course_id = cd.course_id)
+SELECT  c.course_id, c.name, MAX(cd.level) as min_year
+FROM course c 
+INNER JOIN course_dependencies cd USING (course_id)
+GROUP BY c.course_id, c.name
+ORDER BY min_year ASC, c.name ASC;
--- exercises/subqueries/students_with_many_enrolments.sql
+++ exercises/subqueries/students_with_many_enrolments.sql
@@ -9,3 +9,25 @@
 --          - кількістю курсів студента (спадання), потім за іменем студента
 
 -- Рішення:
+WITH count_stud_enrolments as(
+	SELECT  s.student_id,
+		CONCAT(p.first_name, ' ', p.last_name) AS full_name,
+		COUNT (e.course_id) as total_courses
+FROM person p
+	inner join student s USING(person_id)
+	inner join enrolment e ON s.student_id = e.student_id
+GROUP BY s.student_id, p.person_id, p.first_name, p.last_name),
+avg_stud_enrolments AS (
+    SELECT AVG(total_courses) AS avg_total_courses
+    FROM count_stud_enrolments)
+SELECT
+	cse.student_id,
+    cse.full_name,
+    ROUND(cse.total_courses, 2) as course_number,
+    ROUND(ase.avg_total_courses, 2) as avg_number
+FROM count_stud_enrolments cse
+    CROSS JOIN avg_stud_enrolments ase
+WHERE cse.total_courses > ase.avg_total_courses
+ORDER BY 
+    cse.total_courses DESC, 
+    cse.full_name ASC
