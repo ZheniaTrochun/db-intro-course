8a25814597f2ff1e7be6d97e980112b345689123
ARTEM-SERDIUK <serdiuk.artem@lll.kpi.ua>
2026-05-07 19:46:24 +0300

    Виправлено student_average_grade

--- exercises/group_by/student_average_grade.sql
+++ exercises/group_by/student_average_grade.sql
@@ -10,13 +10,16 @@
 --          - за назвою групи, потім за іменем студента
 
 -- Рішення:
-SELECT s.student_id as student_id, p.first_name || ' ' || p.last_name as full_name,
-ROUND(AVG(e.grade)::numeric, 2)::double precision as avg_student_grade, 
-sg.name as group_name,
-ROUND(AVG(AVG(e.grade)) OVER (PARTITION BY sg.group_id)::numeric, 2)::double precision as avg_group_grade
-FROM student s
-JOIN person p ON s.person_id = p.person_id
-LEFT JOIN enrolment e ON s.student_id = e.student_id
-JOIN student_group sg ON s.group_id = sg.group_id
-GROUP BY s.student_id, p.first_name, p.last_name, sg.group_id, sg.name
-ORDER BY group_name, full_name, avg_student_grade, student_id
+WITH student_rating AS (
+    SELECT s.student_id, p.first_name || ' ' || p.last_name AS full_name,
+    sg.name AS group_name, s.group_id, AVG(e.grade) AS student_avg
+    FROM student s
+    JOIN person p ON s.person_id = p.person_id
+    JOIN student_group sg ON s.group_id = sg.group_id
+    LEFT JOIN enrolment e ON s.student_id = e.student_id
+    GROUP BY s.student_id, p.first_name, p.last_name, sg.name, s.group_id)
+
+SELECT sr.student_id, sr.full_name, ROUND(sr.student_avg::numeric, 2)::numeric(38,2) AS avg_student_grade,
+sr.group_name, ROUND(AVG(sr.student_avg) OVER (PARTITION BY sr.group_id)::numeric, 2)::numeric(38,2) AS avg_group_grade
+FROM student_rating sr
+ORDER BY group_name, full_name
