57ac2d9ae93ba90a9e93922a613996399c1c5b56
Ruslana <savchenko.ruslana@lll.kpi.ua>
2026-05-11 21:33:56 +0300

    Update university_activities.sql

--- exercises/joins/university_activities.sql
+++ exercises/joins/university_activities.sql
@@ -11,3 +11,22 @@
 --          - назвою курсу, потім за типом активності, потім за іменем
 
 -- Рішення:
+SELECT p.first_name || ' ' || p.last_name as full_name, 
+       c.name as course_name, 'запис на курс' as activity_type
+  FROM enrolment e
+JOIN student st using(student_id)
+JOIN person p using(person_id)
+JOIN course c ON e.course_id = c.course_id
+WHERE c.status = 'активний'
+
+UNION ALL
+
+SELECT p.first_name || ' ' || p.last_name as full_name, 
+       c.name as course_name, 'викладання курсу' as activity_type
+  FROM course_teacher ct
+JOIN professor pr using(professor_id)
+JOIN person p using(person_id)
+JOIN course c ON ct.course_id = c.course_id
+WHERE c.status = 'активний'
+
+ORDER BY course_name, activity_type, full_name;
