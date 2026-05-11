ae437a9c0ec82f12547df357aef70d2165207d09
Ruslana <savchenko.ruslana@lll.kpi.ua>
2026-05-11 21:42:51 +0300

    Update busiest_teachers.sql

--- exercises/subqueries/busiest_teachers.sql
+++ exercises/subqueries/busiest_teachers.sql
@@ -8,11 +8,17 @@
 --          - кількістю кредитів (спадання), потім за ім'ям
 
 -- Рішення:
-SELECT c.name as course_name,  p.first_name || ' ' || p.last_name as teacher_name,  ct.professor_role as role 
-    FROM course as c
- JOIN course_teacher ct ON ct.course_id = c.course_id
- JOIN professor pr ON ct.professor_id = pr.professor_id 
- JOIN person p ON  pr.person_id = p.person_id 
-WHERE c.status = 'активний'
-  
-ORDER BY course_name, role;
+WITH teacher_credits AS (
+    SELECT pr.professor_id, p.first_name || ' ' || p.last_name as full_name,  SUM(c.credits) as total_credits
+    FROM professor as pr
+    JOIN person p ON pr.person_id = p.person_id
+    JOIN course_teacher ct ON pr.professor_id = ct.professor_id
+    JOIN course c ON ct.course_id = c.course_id
+    GROUP BY pr.professor_id, full_name
+)
+SELECT full_name, total_credits,
+       ROUND(AVG(total_credits) OVER(), 2) as avg_total_credits
+ FROM teacher_credits
+    
+ORDER BY total_credits DESC, full_name
+LIMIT 100;
