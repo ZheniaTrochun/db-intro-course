9fc8495d1040a078713897f8f388f109401a5dac
Ruslana <savchenko.ruslana@lll.kpi.ua>
2026-05-11 22:26:12 +0300

    Update above_average_students_per_group.sql

--- exercises/subqueries/above_average_students_per_group.sql
+++ exercises/subqueries/above_average_students_per_group.sql
@@ -11,26 +11,36 @@
 --          - назвою групи, потім за середнім балом студента (спадання), потім за іменем студента
 
 -- Рішення:
-WITH student_avg AS (
-    SELECT e.student_id, s.group_id, AVG(e.grade) as s_avg
-    FROM enrolment as e
+WITH StudentAverages AS (
+    SELECT 
+        e.student_id, 
+        s.group_id, 
+        AVG(e.grade) AS s_avg
+    FROM enrolment e
     JOIN student s ON e.student_id = s.student_id
+    WHERE e.grade IS NOT NULL
     GROUP BY e.student_id, s.group_id
 ),
-group_avg as (
-    SELECT s.group_id, AVG(e.grade) as g_avg
-    FROM enrolment as e
-    JOIN student s ON e.student_id = s.student_id
-    GROUP BY s.group_id
+GroupAverages AS (
+    SELECT 
+        group_id, 
+        AVG(s_avg) AS g_avg
+    FROM StudentAverages
+    GROUP BY group_id
 )
-SELECT sa.student_id, p.first_name || ' ' || p.last_name as full_name, sg.name as group_name,
-       ROUND(sa.s_avg::numeric, 2) as avg_student_grade,
-       ROUND(ga.g_avg::numeric, 2) as avg_group_grade
-  FROM student_avg as sa
-JOIN group_avg ga ON sa.group_id = ga.group_id
-JOIN student s ON sa.student_id = s.student_id
-JOIN person p ON s.person_id = p.person_id
+SELECT 
+    sa.student_id, 
+    p.first_name || ' ' || p.last_name AS full_name, 
+    sg.name AS group_name,
+    ROUND(sa.s_avg::numeric, 2) AS avg_student_grade,
+    ROUND(ga.g_avg::numeric, 2) AS avg_group_grade
+FROM StudentAverages sa
+JOIN GroupAverages  ga ON sa.group_id = ga.group_id
+JOIN student       s  ON sa.student_id = s.student_id
+JOIN person        p  ON s.person_id = p.person_id
 JOIN student_group sg ON sa.group_id = sg.group_id
 WHERE sa.s_avg > ga.g_avg
-  
-ORDER BY group_name, avg_student_grade DESC, sa.student_id;
+ORDER BY 
+    group_name, 
+    avg_student_grade DESC, 
+    sa.student_id;
