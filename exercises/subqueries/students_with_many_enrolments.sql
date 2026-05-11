8a20ec63c370bccb1e75bf3b5e7547c4ad43b8b7
Andrii <aistratij85@gmail.com>
2026-05-11 16:51:34 +0300

    Refine ordering in students with many enrolments query
    Enhance ordering of results to include student ID in ascending order.

--- exercises/subqueries/students_with_many_enrolments.sql
+++ exercises/subqueries/students_with_many_enrolments.sql
@@ -18,11 +18,17 @@ global_avg_course_count AS (
     SELECT avg(course_number) AS avg_number
     FROM course_count_of_student
 )
-SELECT ccof.student_id AS "student_id", p.first_name || ' ' || p.last_name AS "full_name",
-       ccof.course_number AS "course_number", cast(ROUND(cast(gacc.avg_number AS numeric), 2) AS FLOAT) AS avg_number
+SELECT 
+    ccof.student_id AS "student_id", 
+    p.first_name || ' ' || p.last_name AS "full_name",
+    ccof.course_number AS "course_number", 
+    cast(ROUND(cast(gacc.avg_number AS numeric), 2) AS FLOAT) AS avg_number
 FROM course_count_of_student ccof
-	CROSS JOIN global_avg_course_count gacc
-	JOIN student s ON ccof.student_id = s.student_id
-	JOIN person p ON s.person_id = p.person_id
+    CROSS JOIN global_avg_course_count gacc
+    JOIN student s ON ccof.student_id = s.student_id
+    JOIN person p ON s.person_id = p.person_id
 WHERE ccof.course_number > gacc.avg_number
-ORDER BY ccof.course_number DESC, p.first_name || ' ' || p.last_name;
+ORDER BY 
+    ccof.course_number DESC, 
+    p.first_name || ' ' || p.last_name ASC, 
+    ccof.student_id ASC;
