2da0f2be900a97a27103102ff80c302db9ed696f
Miron44 <kovalenko.miron@lll.kpi.ua>
2026-05-11 20:24:19 +0300

    Виконані вправи

--- exercises/group_by/student_average_grade.sql
+++ exercises/group_by/student_average_grade.sql
@@ -10,3 +10,12 @@
 --          - за назвою групи, потім за іменем студента
 
 -- Рішення:
+SELECT s.student_id AS student_id, p.first_name || ' ' || p.last_name AS full_name,
+ROUND(AVG(e.grade), 2) AS avg_student_grade, sg.name AS group_name, 
+ROUND((AVG(AVG(e.grade)) OVER (PARTITION BY s.group_id)), 2) AS avg_group_grade
+FROM enrolment e
+JOIN student s using(student_id)
+JOIN person p using(person_id)
+JOIN student_group sg ON s.group_id = sg.group_id
+GROUP BY s.student_id, p.first_name, p.last_name, sg.name, s.group_id
+ORDER BY group_name, full_name;
--- exercises/group_by/student_success_rate.sql
+++ exercises/group_by/student_success_rate.sql
@@ -7,3 +7,7 @@
 --          - роком навчання (зростання)
 
 -- Рішення:
+SELECT e.start_year AS student_year, ROUND(AVG(e.grade), 2) AS avg_year_grade
+FROM enrolment e
+GROUP BY student_year
+ORDER BY student_year ASC;
