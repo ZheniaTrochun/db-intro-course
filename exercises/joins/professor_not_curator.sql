b4b384ba19c33b838bebf8b80e99d844bfadd5f5
Ruslana <savchenko.ruslana@lll.kpi.ua>
2026-05-11 12:14:24 +0300

    Update professor_not_curator.sql

--- exercises/joins/professor_not_curator.sql
+++ exercises/joins/professor_not_curator.sql
@@ -7,3 +7,10 @@
 --          - повним іменем викладача
 
 -- Рішення:
+SELECT per.first_name || ' ' || per.last_name as professor_name, pr.job as job 
+  FROM professor pr
+INNER JOIN person per ON pr.person_id = per.person_id
+LEFT JOIN student_group g ON pr.person_id = g.curator_id
+
+WHERE pr.status = 'викладає' AND g.curator_id IS NULL
+ORDER BY professor_name;
