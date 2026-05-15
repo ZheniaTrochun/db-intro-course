fdc4fedb5504b016eefef2302d5012b3a791ec27
xavvitee <leragoncharova1809@gmail.com>
2026-05-16 01:16:10 +0300

    додано завдання students_with_many_enrolments

--- exercises/subqueries/students_with_many_enrolments.sql
+++ exercises/subqueries/students_with_many_enrolments.sql
@@ -9,3 +9,14 @@
 --          - кількістю курсів студента (спадання), потім за іменем студента, потім за ідентифікатор студента
 
 -- Рішення:
+WITH student_courses AS (
+SELECT s.student_id, CONCAT(s.first_name, ' ', s.last_name) AS full_name, COUNT(e.course_id) AS course_number
+FROM student s
+JOIN enrolment e ON s.student_id = e.student_id
+GROUP BY s.student_id, s.first_name, s.last_name
+)
+SELECT student_id, full_name, course_number, ROUND(AVG(course_number) OVER (), 2) AS avg_number FROM student_courses
+WHERE course_number > (
+SELECT AVG(course_number) FROM student_courses
+)
+ORDER BY course_number DESC, full_name ASC, student_id ASC;
