6301f922de74b461d5f7754d2b1bec5b974a6bfd
xavvitee <leragoncharova1809@gmail.com>
2026-05-16 00:22:11 +0300

    додано завдання students_at_risk

--- exercises/joins/students_at_risk.sql
+++ exercises/joins/students_at_risk.sql
@@ -12,3 +12,17 @@
 --          - оцінкою (зростання), потім за назвою групи, потім за іменем студента, потім за назвою курсу
 
 -- Рішення:
+SELECT
+CONCAT(s.first_name, ' ', s.last_name) AS student_name, g.name AS group_name, c.name AS course_name, e.grade,
+CONCAT(p.first_name, ' ', p.last_name) AS lecturer_name
+FROM student s
+JOIN student_group g ON s.group_id = g.group_id
+JOIN enrolment e ON s.student_id = e.student_id
+JOIN course c ON e.course_id = c.course_id
+JOIN course_teacher ct ON c.course_id = ct.course_id
+JOIN professor prof ON ct.professor_id = prof.professor_id
+JOIN person p ON prof.person_id = p.person_id
+WHERE e.grade IS NOT NULL
+AND e.grade < 60
+AND ct.professor_role = 'лектор'
+ORDER BY e.grade ASC, group_name ASC, student_name ASC, course_name ASC;
