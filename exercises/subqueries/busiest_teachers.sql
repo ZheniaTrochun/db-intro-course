99ceddb4805d68c4867cd7094f799627cde3315d
xavvitee <leragoncharova1809@gmail.com>
2026-05-16 00:51:19 +0300

    додано завдання busiest_teachers

--- exercises/subqueries/busiest_teachers.sql
+++ exercises/subqueries/busiest_teachers.sql
@@ -8,3 +8,16 @@
 --          - кількістю кредитів (спадання), потім за ім'ям
 
 -- Рішення:
+WITH professor_credits AS (
+SELECT CONCAT(p.first_name, ' ', p.last_name) AS full_name, SUM(c.credits) AS total_credits
+FROM person p
+JOIN professor prof ON p.person_id = prof.person_id
+JOIN course_teacher ct ON prof.professor_id = ct.professor_id
+JOIN course c ON ct.course_id = c.course_id
+GROUP BY p.person_id, p.first_name, p.last_name
+)
+SELECT full_name, total_credits,
+ROUND(AVG(total_credits) OVER (), 2) AS avg_total_credits
+FROM professor_credits
+ORDER BY total_credits DESC, full_name ASC
+LIMIT 100;
