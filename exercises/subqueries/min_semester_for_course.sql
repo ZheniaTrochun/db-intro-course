3f196e4c8d862c10f9e6746f2094f6e4b645ecbd
xavvitee <leragoncharova1809@gmail.com>
2026-05-16 01:05:58 +0300

    додано завдання min_semester_for_course

--- exercises/subqueries/min_semester_for_course.sql
+++ exercises/subqueries/min_semester_for_course.sql
@@ -8,3 +8,19 @@
 --          - мінімальним роком (зростання), потім за назвою курсу
 
 -- Рішення:
+WITH RECURSIVE course_dependencies AS (
+SELECT course_id, 1 AS LEVEL
+FROM course c
+WHERE NOT EXISTS (
+SELECT 1 
+FROM course_prerequisite p 
+WHERE p.course_id = c.course_id)
+UNION ALL
+SELECT p.course_id AS course_id, cd.level + 1 AS LEVEL
+FROM course_prerequisite p
+INNER JOIN course_dependencies cd ON p.prerequisite_course_id = cd.course_id)
+SELECT  c.course_id, c.name, MAX(cd.level) AS min_year
+FROM course c 
+INNER JOIN course_dependencies cd USING (course_id)
+GROUP BY c.course_id, c.name
+ORDER BY min_year ASC, c.name ASC;
