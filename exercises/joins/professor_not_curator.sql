26ed1e44f145a5d89673cc807869febb77595bae
xavvitee <leragoncharova1809@gmail.com>
2026-05-16 00:09:39 +0300

    додано завдання professor_not_curator

--- exercises/joins/professor_not_curator.sql
+++ exercises/joins/professor_not_curator.sql
@@ -7,3 +7,8 @@
 --          - повним іменем викладача
 
 -- Рішення:
+SELECT p.first_name ||' '|| p.last_name as professor_name, p2.job FROM person p 
+JOIN professor p2 ON p.person_id = p2.person_id
+WHERE p2.professor_id NOT IN (select p3.professor_id FROM professor p3
+JOIN student_group g ON p3.professor_id = g.curator_id) AND p2.status = 'викладає'
+ORDER BY professor_name
