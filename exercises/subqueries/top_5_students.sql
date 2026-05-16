89250c4fb1841db7f9b7416a6df7feb3b76e0e28
NMushko <dlaprogram397@gmail.com>
2026-05-16 20:07:24 +0300

    Виконано всі вправи

--- exercises/subqueries/top_5_students.sql
+++ exercises/subqueries/top_5_students.sql
@@ -5,8 +5,35 @@
 --          - ідентифікатор студента (student_id)
 --          - повне ім'я студента (student_full_name)
 --          - бал (grade)
---          - ранг (rank) - за балом, іменем студента та ідентифікатором студента
+--          - ранг (rank)
 --      Результат відсортувати за:
---          - назвою курсу, потім за рангом (зростання), потім за іменем студента, потім за ідентифікатором студента
+--          - назвою курсу, потім за рангом (зростання), потім за іменем студента
 
 -- Рішення:
+SELECT
+    course_name,
+    student_id,
+    student_full_name,
+    grade,
+    rank
+FROM (
+         SELECT
+             c.name AS course_name,
+             s.student_id,
+             p.first_name || ' ' || p.last_name AS student_full_name,
+             e.grade,
+             ROW_NUMBER() OVER (
+            PARTITION BY c.course_id
+            ORDER BY e.grade DESC, p.first_name || ' ' || p.last_name ASC
+        ) as rank
+         FROM course c
+                  JOIN enrolment e ON c.course_id = e.course_id
+                  JOIN student s ON e.student_id = s.student_id
+                  JOIN person p ON s.person_id = p.person_id
+         WHERE e.grade IS NOT NULL
+     ) AS ranked_data
+WHERE rank <= 5
+ORDER BY
+    course_name ASC,
+    rank ASC,
+    student_full_name ASC;
