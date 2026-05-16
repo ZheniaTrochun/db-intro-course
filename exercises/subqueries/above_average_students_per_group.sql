aefcf578cdf75ebdd476e67852ccda5306701c58
xavvitee <leragoncharova1809@gmail.com>
2026-05-16 16:11:25 +0300

    додано above_average_students_per_group

--- exercises/subqueries/above_average_students_per_group.sql
+++ exercises/subqueries/above_average_students_per_group.sql
@@ -11,25 +11,23 @@
 --          - назвою групи, потім за середнім балом студента (спадання), потім за іменем студента
 
 -- Рішення:
-WITH student_avg AS (
-SELECT s.student_id, CONCAT(s.first_name, ' ', s.last_name) AS full_name, s.group_id, AVG(e.grade) AS avg_student_grade
-FROM student s
-JOIN enrolment e ON s.student_id = e.student_id
-WHERE e.grade IS NOT NULL
-GROUP BY s.student_id, s.first_name, s.last_name, s.group_id
+WITH students_avg_score AS (
+    SELECT student_id, avg(grade) AS raw_avg
+    FROM enrolment e
+    GROUP BY e.student_id
 ),
-group_avg AS (
-SELECT s.group_id, AVG(e.grade) AS avg_group_grade
-FROM student s
-JOIN enrolment e ON s.student_id = e.student_id
-WHERE e.grade IS NOT NULL
-GROUP BY s.group_id
+groups_avg_score AS (
+    SELECT s.group_id, avg(e.grade) AS raw_group_avg
+    FROM student s
+        JOIN enrolment e ON s.student_id = e.student_id
+    GROUP BY s.group_id
 )
-SELECT sa.student_id, sa.full_name, g.name AS group_name,
-ROUND(sa.avg_student_grade, 2) AS avg_student_grade,
-ROUND(ga.avg_group_grade, 2) AS avg_group_grade
-FROM student_avg sa
-JOIN group_avg ga ON sa.group_id = ga.group_id
-JOIN student_group g ON sa.group_id = g.group_id
-WHERE sa.avg_student_grade > ga.avg_group_grade
-ORDER BY group_name ASC, avg_student_grade DESC, full_name ASC;
+SELECT s.student_id AS "student_id", p.first_name || ' ' || p.last_name AS "full_name", sg.name AS "group_name",
+    ROUND(sa.raw_avg, 2) AS "avg_student_grade", ROUND(ga.raw_group_avg, 2) AS "avg_group_grade"
+FROM student s
+    JOIN person p ON s.person_id = p.person_id
+    JOIN student_group sg ON s.group_id = sg.group_id
+    JOIN students_avg_score sa ON s.student_id = sa.student_id
+    JOIN groups_avg_score ga ON s.group_id = ga.group_id
+WHERE ROUND(sa.raw_avg, 2) > ROUND(ga.raw_group_avg, 2)
+ORDER BY sg.name, sa.raw_avg DESC, p.first_name || ' ' || p.last_name;
