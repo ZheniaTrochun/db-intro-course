f996f83bf86875d7ca8b698e332e8b97a3f3238d
Anton <koreniako.anton@lll.kpi.ua>
2026-05-17 10:46:08 +0300

    Done

--- exercises/joins/students_at_risk.sql
+++ exercises/joins/students_at_risk.sql
@@ -12,3 +12,21 @@
 --          - оцінкою (зростання), потім за назвою групи, потім за іменем студента, потім за назвою курсу
 
 -- Рішення:
+
+ІО-41 Кореняко Антон
+
+select p.first_name || ' ' || p.last_name as student_name, sg.name as group_name,
+c.name as course_name, e.grade as grade, ppr.first_name || ' ' || ppr.last_name as lecturer_name
+
+from enrolment e
+join student s on e.student_id = s.student_id
+join person p on s.person_id = p.person_id
+join student_group sg on s.group_id = sg.group_id
+join course c on e.course_id = c.course_id
+join course_teacher ct on c.course_id = ct.course_id
+join professor pr on ct.professor_id = pr.professor_id
+join person ppr on pr.person_id = ppr.person_id
+
+where e.grade < 60 and e.grade is not null and ct.professor_role = 'лектор'
+
+order by grade, group_name, student_name, course_name
