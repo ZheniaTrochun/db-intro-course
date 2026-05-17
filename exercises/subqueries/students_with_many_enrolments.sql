68035b81683a17a49ccfac1a8732e371c68d8cb8
Anton <koreniako.anton@lll.kpi.ua>
2026-05-17 20:56:12 +0300

    Done

--- exercises/subqueries/students_with_many_enrolments.sql
+++ exercises/subqueries/students_with_many_enrolments.sql
@@ -12,16 +12,23 @@
 
 --ІО-41 Кореняко Антон
 
-select s.student_id as student_id, p.first_name || ' ' || p.last_name as full_name,
-count(e.course_id) as course_number, (select round(avg(counts.cnt), 2)
-    from (select count(course_id) as cnt
-       from enrolment group by student_id) as counts) as avg_number
-from student s
-join person p on s.person_id = p.person_id
-join enrolment e on s.student_id = e.student_id
-group by s.student_id, p.first_name, p.last_name
-having count(e.course_id) > (select avg(counts.cnt)
-    from (select count(course_id) as cnt
-       from enrolment group by student_id) as counts)
-
-order by course_number desc, full_name, s.student_id
+SELECT
+    s.student_id,
+    p.first_name  ' '
+ p.last_name AS full_name,
+    COUNT(e.course_id) AS course_number,
+    CAST((SELECT ROUND(AVG(counts.cnt)::numeric, 2)
+          FROM (SELECT COUNT(course_id) AS cnt FROM enrolment GROUP BY student_id) AS counts
+         ) AS DOUBLE PRECISION) AS avg_number
+FROM student s
+JOIN person p ON s.person_id = p.person_id
+JOIN enrolment e ON s.student_id = e.student_id
+GROUP BY s.student_id, p.first_name, p.last_name
+HAVING COUNT(e.course_id) > (
+    SELECT AVG(counts.cnt)
+    FROM (SELECT COUNT(course_id) AS cnt FROM enrolment GROUP BY student_id) AS counts
+)
+ORDER BY
+    course_number DESC,
+    full_name ASC,
+    s.student_id ASC;
