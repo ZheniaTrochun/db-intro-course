f5bf152856f3d733ee44df21c446131735f90bc2
Anton <koreniako.anton@lll.kpi.ua>
2026-05-17 20:55:31 +0300

    Done

--- exercises/group_by/successful_groups.sql
+++ exercises/group_by/successful_groups.sql
@@ -11,13 +11,16 @@
 
 --ІО-41 Кореняко Антон
 
-select sg.name as group_name, count(distinct s.student_id) as student_count,
-round(avg(e.grade::numeric), 2) as avg_grade
-
-from student_group sg
-join student s on sg.group_id = s.group_id
-left join enrolment e on s.student_id = e.student_id
-
-group by sg.group_id, sg.name
-having round(avg(e.grade::numeric), 2) > 75
-order by avg_grade desc, group_name
+SELECT
+    sg.name AS group_name,
+    COUNT(DISTINCT s.student_id) AS student_count,
+    CAST(ROUND(AVG(e.grade)::numeric, 2) AS DOUBLE PRECISION) AS avg_grade
+FROM student_group sg
+JOIN student s ON sg.group_id = s.group_id
+JOIN enrolment e ON s.student_id = e.student_id
+WHERE e.grade IS NOT NULL
+GROUP BY sg.group_id, sg.name
+HAVING ROUND(AVG(e.grade)::numeric, 2) > 75
+ORDER BY
+    avg_grade DESC,
+    group_name ASC;
