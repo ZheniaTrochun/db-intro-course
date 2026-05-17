cca7c453775545042c2de3e1a7785fe6d4f4a962
Anton <koreniako.anton@lll.kpi.ua>
2026-05-17 21:31:30 +0300

    Done

--- exercises/group_by/student_average_grade.sql
+++ exercises/group_by/student_average_grade.sql
@@ -22,11 +22,9 @@ SELECT
     CAST(ROUND(AVG(AVG(e.grade)) OVER (PARTITION BY s.group_id)::numeric, 2) AS DOUBLE PRECISION) AS avg_group_grade
 FROM student s
 JOIN person p ON s.person_id = p.person_id
-JOIN student_group sg ON s.group_id = sg.group_id
 LEFT JOIN enrolment e ON s.student_id = e.student_id
-GROUP BY s.student_id, p.first_name, p.last_name, sg.name, s.group_id
+JOIN student_group sg ON s.group_id = sg.group_id
+GROUP BY s.student_id, p.first_name, p.last_name, sg.group_id, sg.name
 ORDER BY
     group_name ASC,
-    full_name ASC,
-    avg_student_grade ASC,
-    s.student_id ASC;
+    full_name ASC;
