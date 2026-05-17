79317c3ad542fc9a6ccdce87dc9487944d1196d2
Anton <koreniako.anton@lll.kpi.ua>
2026-05-17 11:00:42 +0300

    Done

--- exercises/subqueries/top_5_students.sql
+++ exercises/subqueries/top_5_students.sql
@@ -10,3 +10,23 @@
 --          - назвою курсу, потім за рангом (зростання), потім за іменем студента, потім за ідентифікатором студента
 
 -- Рішення:
+
+ІО-41 Кореняко Антон
+
+  with ranklist as (
+    select c.name as course_name, e.student_id, p.first_name || ' ' || p.last_name as full_name,
+    e.grade, row_number() over (partition by c.course_id
+    order by e.grade desc nulls last, (p.first_name || ' ' || p.last_name)) as student_rank
+    from enrolment e
+    join course c on e.course_id = c.course_id
+    join student s on e.student_id = s.student_id
+    join person p on s.person_id = p.person_id)
+
+select rl.course_name, rl.student_id, rl.full_name as student_full_name, rl.grade as grade,
+rl.student_rank as rank
+
+from ranklist rl
+
+where rl.student_rank < 6
+
+order by course_name, student_rank, student_full_name
