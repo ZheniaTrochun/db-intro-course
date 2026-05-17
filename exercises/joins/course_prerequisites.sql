94d10a81d9e85d7387368e003a2bff82fec43cb1
Anton <koreniako.anton@lll.kpi.ua>
2026-05-17 10:41:58 +0300

    Done

--- exercises/joins/course_prerequisites.sql
+++ exercises/joins/course_prerequisites.sql
@@ -8,3 +8,14 @@
 --          - назвою курсу, потім за назвою пре-реквізиту
 
 -- Рішення:
+
+
+ІО-41 Кореняко Антон
+
+select c.name as course_name, c2.name as prerequisite_name
+
+from course c
+left join course_prerequisite cp on c.course_id = cp.course_id
+left join course c2 on cp.prerequisite_course_id = c2.course_id
+
+order by course_name, prerequisite_name
