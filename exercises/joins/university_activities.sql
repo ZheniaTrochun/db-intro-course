31f3f53bfe151201a6d1bcc0bc2b90f4750b2d43
Anton <koreniako.anton@lll.kpi.ua>
2026-05-17 10:47:30 +0300

    Done

--- exercises/joins/university_activities.sql
+++ exercises/joins/university_activities.sql
@@ -11,3 +11,28 @@
 --          - назвою курсу, потім за типом активності, потім за іменем
 
 -- Рішення:
+
+ІО-41 Кореняко Антон
+
+select p.first_name || ' ' || p.last_name as full_name, c.name as course_name,
+'ЗАПИС НА КУРС' as activity_type
+
+from enrolment e
+join student s on e.student_id = s.student_id
+join person p on s.person_id = p.person_id
+join course c on e.course_id = c.course_id
+where c.status = 'АКТИВНИЙ'
+
+union all
+
+select p.first_name || ' ' || p.last_name as full_name, c.name as course_name,
+'ВИКЛАДАННЯ КУРСУ' as activity_type
+
+from course_teacher ct
+join professor pr on ct.professor_id = pr.professor_id
+join person p on pr.person_id = p.person_id
+join course c on ct.course_id = c.course_id
+
+where c.status = 'АКТИВНИЙ'
+
+order by course_name, activity_type, full_name
