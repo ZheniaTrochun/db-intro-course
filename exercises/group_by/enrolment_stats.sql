43f1616bdcab298e340c7683e1191b1e50b77ca2
Oksana-dev <ostroginskaa@gmail.com>
2026-05-17 17:50:08 +0300

    Add SQL query to group enrolment stats by year

--- exercises/group_by/enrolment_stats.sql
+++ exercises/group_by/enrolment_stats.sql
@@ -9,3 +9,12 @@
 --          - роком навчання (зростання)
 
 -- Рішення:
+select 
+start_year as student_year,
+count(distinct course_id) as number_of_courses,
+count(student_id) as number_of_enrolments,
+count(grade) as number_of_students_with_grade
+from enrolment
+group by start_year
+order by student_year
+
