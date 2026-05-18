6c4b9795c2502eba091e2ade664037e5674f6f23
Anton <koreniako.anton@lll.kpi.ua>
2026-05-18 19:52:32 +0300

    Done

--- exercises/group_by/student_average_grade.sql
+++ exercises/group_by/student_average_grade.sql
@@ -14,26 +14,33 @@
 
 --ІО-41 Кореняко Антон
 
-WITH StudentGrades AS (
+WITH StudentAverages AS (
 
     SELECT
         s.student_id,
         CONCAT(p.first_name, ' ', p.last_name) AS full_name,
         sg.name AS group_name,
         s.group_id,
-        e.grade
+        AVG(e.grade) AS avg_student_grade
     FROM student s
     JOIN person p ON s.person_id = p.person_id
     LEFT JOIN student_group sg ON s.group_id = sg.group_id
     LEFT JOIN enrolment e ON s.student_id = e.student_id
+    GROUP BY
+        s.student_id,
+        p.first_name,
+        p.last_name,
+        sg.name,
+        s.group_id
 )
-SELECT DISTINCT
+
+SELECT
     student_id,
     full_name,
-    ROUND(AVG(grade) OVER (PARTITION BY student_id), 2) AS avg_student_grade,
+    ROUND(avg_student_grade, 2) AS avg_student_grade,
     group_name,
-    ROUND(AVG(grade) OVER (PARTITION BY group_id), 2) AS avg_group_grade
-FROM StudentGrades
+    ROUND(AVG(avg_student_grade) OVER (PARTITION BY group_id), 2) AS avg_group_grade
+FROM StudentAverages
 ORDER BY
     group_name,
     full_name;
