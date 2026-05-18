4085be0764d031ab5e9393ee7e11336c421e984b
Anton <koreniako.anton@lll.kpi.ua>
2026-05-18 19:57:17 +0300

    Done

--- exercises/group_by/student_average_grade.sql
+++ exercises/group_by/student_average_grade.sql
@@ -14,33 +14,22 @@
 
 --ІО-41 Кореняко Антон
 
-WITH StudentAverages AS (
-
-    SELECT
-        s.student_id,
-        CONCAT(p.first_name, ' ', p.last_name) AS full_name,
-        sg.name AS group_name,
-        s.group_id,
-        AVG(e.grade) AS avg_student_grade
-    FROM student s
-    JOIN person p ON s.person_id = p.person_id
-    LEFT JOIN student_group sg ON s.group_id = sg.group_id
-    LEFT JOIN enrolment e ON s.student_id = e.student_id
-    GROUP BY
-        s.student_id,
-        p.first_name,
-        p.last_name,
-        sg.name,
-        s.group_id
-)
-
 SELECT
-    student_id,
-    full_name,
-    ROUND(avg_student_grade, 2) AS avg_student_grade,
-    group_name,
-    ROUND(AVG(avg_student_grade) OVER (PARTITION BY group_id), 2) AS avg_group_grade
-FROM StudentAverages
+    s.student_id,
+    CONCAT(p.first_name, ' ', p.last_name) AS full_name,
+    ROUND(AVG(e.grade), 2) AS avg_student_grade,
+    sg.name AS group_name,
+    ROUND(AVG(AVG(e.grade)) OVER (PARTITION BY s.group_id), 2) AS avg_group_grade
+FROM student s
+JOIN person p ON s.person_id = p.person_id
+LEFT JOIN student_group sg ON s.group_id = sg.group_id
+LEFT JOIN enrolment e ON s.student_id = e.student_id
+GROUP BY
+    s.student_id,
+    p.first_name,
+    p.last_name,
+    sg.name,
+    s.group_id
 ORDER BY
     group_name,
     full_name;
