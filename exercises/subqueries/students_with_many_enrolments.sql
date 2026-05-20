cc541050e58b771797d68e28f3481bbf5582ba56
Oksana-dev <ostroginskaa@gmail.com>
2026-05-21 00:03:23 +0300

    Add SQL query to find students with many enrolments

--- exercises/subqueries/students_with_many_enrolments.sql
+++ exercises/subqueries/students_with_many_enrolments.sql
@@ -9,3 +9,22 @@
 --          - кількістю курсів студента (спадання), потім за іменем студента, потім за ідентифікатор студента
 
 -- Рішення:
+with student_courses as (
+  select 
+  s.student_id,
+  p.first_name || ' ' || p.last_name as full_name,
+  count(e.course_id) as course_number
+  from student s
+  join person p on s.person_id = p.person_id
+  join enrolment e on s.student_id = e.student_id
+  group by s.student_id, p.first_name, p.last_name
+)
+
+select 
+  student_id,
+  full_name,
+  course_number,
+  round(avg(course_number) over ()::numeric, 2)::float as avg_number
+from student_courses
+where course_number > (select avg(course_number) from student_courses)
+order by course_number desc, full_name asc, student_id asc
