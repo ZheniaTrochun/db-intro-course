4704e8b4f2bd36d198134c05d3a6472584ff0b63
Oksana-dev <ostroginskaa@gmail.com>
2026-05-20 23:53:54 +0300

    Implement query to find busiest teachers by credits

--- exercises/subqueries/busiest_teachers.sql
+++ exercises/subqueries/busiest_teachers.sql
@@ -8,3 +8,23 @@
 --          - кількістю кредитів (спадання), потім за ім'ям
 
 -- Рішення:
+with teacher_credits as (
+select 
+  p.first_name || ' ' || p.last_name as full_name,
+  sum(c.credits) as total_credits
+  from professor prof
+  join person p on prof.person_id = p.person_id
+  join course_teacher ct on prof.professor_id = ct.professor_id
+  join course c on ct.course_id = c.course_id
+  group by prof.professor_id, p.first_name, p.last_name
+)
+
+select 
+  full_name,
+  total_credits,
+  round(avg(total_credits) over ()::numeric, 2)::float as avg_total_credits
+from teacher_credits
+order by 
+  total_credits desc, 
+  full_name asc
+limit 100
