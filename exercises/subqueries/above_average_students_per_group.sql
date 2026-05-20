b243b0d42cd6d715b13281ccbc27be304524bc69
Oksana-dev <ostroginskaa@gmail.com>
2026-05-21 00:16:45 +0300

    Refactor SQL query for average grades and sorting

--- exercises/subqueries/above_average_students_per_group.sql
+++ exercises/subqueries/above_average_students_per_group.sql
@@ -11,12 +11,19 @@
 --          - назвою групи, потім за середнім балом студента (спадання), потім за іменем студента
 
 -- Рішення:
-select s.student_id, p.first_name, p.last_name, s.group_id, avg(e.grade) as avg_grade
+select s.student_id, p.first_name || ' ' || p.last_name as full_name, sg.name as group_name, round(avg(e.grade), 2) as avg_student_grade, (
+select round(avg(e2.grade), 2)
+from student s2
+join enrolment e2 on s2.student_id = e2.student_id
+where s2.group_id = s.group_id
+and e2.grade is not null
+) as avg_group_grade
 from student s
 join person p on s.person_id = p.person_id
+join student_group sg on s.group_id = sg.group_id
 join enrolment e on s.student_id = e.student_id
 where e.grade is not null
-group by s.student_id, p.first_name, p.last_name, s.group_id
+group by s.student_id, p.first_name, p.last_name, sg.name, s.group_id
 having avg(e.grade) > (
 select avg(e2.grade)
 from student s2
@@ -24,4 +31,4 @@ join enrolment e2 on s2.student_id = e2.student_id
 where s2.group_id = s.group_id
 and e2.grade is not null
 )
-order by s.group_id, avg_grade desc
+order by sg.name, avg_student_grade desc
