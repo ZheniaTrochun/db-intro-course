1edb04e5357cac5f862d13f1b97373094330f9cc
Oksana-dev <ostroginskaa@gmail.com>
2026-05-20 23:52:09 +0300

    Refactor SQL query for above average students

--- exercises/subqueries/above_average_students_per_group.sql
+++ exercises/subqueries/above_average_students_per_group.sql
@@ -13,37 +13,36 @@
 -- Рішення:
 with student_avg as (
   select 
-    s.student_id,
-    s.group_id,
-    p.first_name || ' ' || p.last_name as full_name,
-    avg(e.grade) as avg_student_grade
+  s.student_id,
+  s.group_id,
+  p.first_name || ' ' || p.last_name as full_name,
+  avg(e.grade) as avg_student_grade
   from student s
   join person p on s.person_id = p.person_id
   join enrolment e on s.student_id = e.student_id
+  where e.grade is not null
   group by s.student_id, s.group_id, p.first_name, p.last_name
 ),
 
 group_avg as (
   select 
-    s.group_id,
-    g.name as group_name,
-    avg(e.grade) as avg_group_grade
+  s.group_id,
+  g.name as group_name,
+  avg(e.grade) as avg_group_grade
   from student s
   join student_group g on s.group_id = g.group_id
   join enrolment e on s.student_id = e.student_id
+  where e.grade is not null
   group by s.group_id, g.name
 )
 
 select 
-  sa.student_id,
-  sa.full_name,
-  ga.group_name,
-  round(sa.avg_student_grade::numeric, 2)::float as avg_student_grade,
-  round(ga.avg_group_grade::numeric, 2)::float as avg_group_grade
+sa.student_id,
+sa.full_name,
+ga.group_name,
+round(sa.avg_student_grade::numeric, 2)::float as avg_student_grade,
+round(ga.avg_group_grade::numeric, 2)::float as avg_group_grade
 from student_avg sa
 join group_avg ga on sa.group_id = ga.group_id
 where sa.avg_student_grade > ga.avg_group_grade
-order by 
-  ga.group_name asc, 
-  sa.avg_student_grade desc, 
-  sa.full_name asc
+order by ga.group_name asc, sa.avg_student_grade desc, sa.full_name asc
